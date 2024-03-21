a49bc3c4052fc96f0b2d4e25555456669c7077e6
Cindy <143493823+CindyA254@users.noreply.github.com>
2024-03-21 18:25:55 +0300

    Add files via upload

---
+++ wk3controlflowassignment.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  // Prompt user for input
+  print("Enter a number: ");
+  String userInput = stdin.readLineSync()!;
+  int number = int.parse(userInput);
+
+  // Check the number and print the appropriate message
+  if (number > 10) {
+    print("Your number is greater than 10");
+  } else if (number < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
